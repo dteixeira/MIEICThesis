8e796289c51cf264f1c06f92e081414e4883c449
Diogo Teixeira <diogo.andre.teixeira@gmail.com>
2014-06-04 16:51:56 +0100

    Build script updated.

--- diss/build.sh
+++ diss/build.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
-# Remove existent PDF file.
-rm report.pdf 2> /dev/null
-
 # Build report.
 if [[ "$#" -eq 1 ]]; then
-
   if [[ "$1" =~ s|S ]]; then
+    # Remove existent PDF file.
+    rm report.pdf 2> /dev/null
     make report._show
+  elif [[ "$1" =~ m|M ]]; then
+    # Make a copy of the PDF file.
+    if [ "$(uname)" == "Darwin" ]; then
+      MD5=$(md5 -r ./report.pdf | awk '{ print $1; }')
+    elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+      MD5=$(md5 -r ./report.pdf | awk '{ print $1; }')
+    fi
+    cp ./report.pdf "$MD5.pdf"
   else
     echo "Invalid argument"
   fi
-
 else
+  # Remove existent PDF file.
+  rm report.pdf 2> /dev/null
   make report
 fi
 
