bd5e63b4b39c83e238ac63a54141050a13010af7
Diogo Teixeira <diogo.andre.teixeira@gmail.com>
2014-07-01 14:21:48 +0100

    Presentation added.

---
+++ presentation/build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Build report.
+if [[ "$#" -eq 1 ]]; then
+  if [[ "$1" =~ s|S ]]; then
+    # Remove existent PDF file.
+    rm presentation.pdf 2> /dev/null
+    make presentation
+    cp presentation.pdf view.pdf
+    open view.pdf
+  else
+    echo "Invalid argument"
+  fi
+else
+  rm presentation.pdf 2> /dev/null
+  make presentation
+  cp presentation.pdf view.pdf
+fi
+
+# Clean compilation files.
+make clean
